a604615d1717665b5aff732ec1e12f9378cbc633
ChrisWhealy <chris@whealy.com>
2024-11-07 08:12:50 +0000

    Optimise coding

--- sha256sum.mjs
+++ sha256sum.mjs
@@ -2,25 +2,22 @@ import { readFileSync } from "fs"
 import { WASI } from "wasi"
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-//  Define WASI environment
-const wasi = new WASI({
-  args: process.argv,
-  version: "unstable",
-  preopens: { ".": process.cwd() },    // Appears as fd 3 when using WASI path_open
-})
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // Instantiate the WASM module
-const startSha256Wasm =
-  async pathToWasmFile => {
+const startWasm =
+  async pathToWasmMod => {
+    //  Define WASI environment
+    const wasi = new WASI({
+      args: process.argv,
+      version: "unstable",
+      preopens: { ".": process.cwd() },    // This directory is available as fd 3 when calling WASI path_open
+    })
+
     let { instance } = await WebAssembly.instantiate(
-      new Uint8Array(readFileSync(pathToWasmFile)),
-      {
-        wasi_snapshot_preview1: wasi.wasiImport,
-      },
+      new Uint8Array(readFileSync(pathToWasmMod)),
+      { wasi_snapshot_preview1: wasi.wasiImport },
     )
 
     wasi.start(instance)
   }
 
-await startSha256Wasm("./bin/sha256_opt.wasm")
+await startWasm("./bin/sha256_opt.wasm")
